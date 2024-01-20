9b371d5681a150bd3384d20379a2ce63ac346f8e
Wookji-Yoon <wookji.yoon@gmail.com>
2024-01-20 14:37:20 +0900

    Finish_DocumentGPT

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="FullStackGPT Home", page_icon="🤖")
+
+st.title("FullStackGPT Home")
--- main.py
+++
@@ -1,4 +0,0 @@
-print("hi")
-
-apple = "hi"
-apple = "hi"
